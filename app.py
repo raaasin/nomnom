1a15459e6f55c1e6c8012de0523ab305203e4530
raaasin <nisarvskp@gmail.com>
2023-09-09 12:20:35 +0530

    something

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import numpy as np
-import pandas as pd
 from flask import Flask, render_template, request
 from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
 
 app = Flask(__name__)
 
 # Load data from CSV
 data = pd.read_csv('restaurants.csv')
 
-# Mapping dictionaries
+# Dictionary to map categorical values to numerical labels
 category_mapping = {
     'Cheap': 0,
     'Medium': 1,
@@ -24,32 +24,31 @@ category_mapping = {
     'Moderate': 2
 }
 
-collab_similarities = np.array([0.8, 0.6, 0.5, 0.7, 0.9])  # Similarity scores for collaborative filtering
-weights = np.array([0.7, 0.3])  # Adjust weights based on preference
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user_profile = np.array([
-            int(request.form['budget']),
-            int(request.form['aesthetics']),
-            int(request.form['type']),
-            int(request.form['diet'])
-        ])
-        
-        content_similarities = cosine_similarity(
-            user_profile.reshape(1, -1),
-            data[['Budget', 'Aesthetics', 'Type', 'Diet']].applymap(lambda x: category_mapping[x])
-        )
-        
+        mood = int(request.form['mood'])
+        budget = int(request.form['budget'])
+        aesthetics = int(request.form['aesthetics'])
+        type = int(request.form['type'])
+        diet = int(request.form['diet'])
+
+        user_profile = np.array([budget, aesthetics, type, diet])
+
+        content_similarities = cosine_similarity(user_profile.reshape(1, -1), data[['Budget', 'Aesthetics', 'Type', 'Diet']].applymap(lambda x: category_mapping[x]))
+
+        collab_similarities = np.array([0.8, 0.6, 0.5, 0.7, 0.9])  # Similarity scores for collaborative filtering
+        collab_similarities = collab_similarities.reshape((1, 5))  # Reshape collab_similarities to (1, 5)
+        weights = np.array([0.7, 0.3])  # Adjust weights based on preference
         hybrid_scores = (content_similarities * weights[0]) + (collab_similarities * weights[1])
+
         ranked_restaurants = np.argsort(hybrid_scores[0])[::-1]
         top_recommended = ranked_restaurants[:3]
-        
-        recommended_restaurants = data.iloc[top_recommended]['Restaurant'].tolist()
-        
+
+        recommended_restaurants = [data.iloc[idx]['Restaurant'] for idx in top_recommended]
+
         return render_template('index.html', recommendations=recommended_restaurants)
-    
+
     return render_template('index.html', recommendations=[])
 
 if __name__ == '__main__':
