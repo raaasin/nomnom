41359f96b745d6e48de31d7f4c1cfe1ece4bfaee
raaasin <nisarvskp@gmail.com>
2023-10-06 23:28:13 +0530

    js needs to be fixed to fetch data

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import numpy as np
 from flask import Flask, render_template, request, jsonify
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
-
+from algo import recommend_restaurants
 app = Flask(__name__)
-
-data = pd.read_csv('restaurants.csv')
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -16,31 +13,10 @@ def index():
             aesthetics = int(request.form['aesthetics'])
             type = int(request.form['type'])
             diet = int(request.form['diet'])
-
-            user_profile = np.array([budget, aesthetics, type, diet])
-
-            # Map categorical values to numerical labels
-            user_profile = np.array([category_mapping.get(category, 0) for category in user_profile])
-
-            # Calculate cosine similarity
-            content_similarities = cosine_similarity([user_profile], data[['Budget', 'Aesthetics', 'Type', 'Diet']].applymap(lambda x: category_mapping[x]))
-            content_similarities = content_similarities.flatten()
-
-            collab_similarities = np.random.rand(len(data))  # Random similarity scores for collaborative filtering
-
-            weights = np.array([0.7, 0.3])  # Adjust weights based on preference
-            hybrid_scores = (content_similarities * weights[0]) + (collab_similarities * weights[1])
-
-            ranked_restaurants = np.argsort(hybrid_scores)[::-1]
-            top_recommended = ranked_restaurants[:3]
-
-            recommended_restaurants = [data.iloc[idx]['Restaurant'] for idx in top_recommended]
-
-            return jsonify({"recommendations": recommended_restaurants})
+            return jsonify({recommend_restaurants(type,mood,budget,aesthetics,type,diet)})
         except Exception as e:
             return jsonify({"error": str(e)})
 
     return render_template('index.html', recommendations=[])
-
 if __name__ == '__main__':
     app.run(debug=True)
